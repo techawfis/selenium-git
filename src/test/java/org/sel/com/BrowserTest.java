7ab85dc26c0e1fce530fc4b2de800aabf077a7cd
sulai <sulai135>
2025-04-09 16:32:40 -0700

    Branch: Selenium_branch_01, change w.r.to browseractions

--- src/test/java/org/sel/com/BrowserTest.java
+++ src/test/java/org/sel/com/BrowserTest.java
@@ -5,9 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.stream.Stream;
+
 public class BrowserTest implements Runnable {
 
     private final String browserType;
+    WebDriver driver;
 
     // Constructor to specify the browser type
     public BrowserTest(String browserType) {
@@ -16,7 +19,7 @@ public class BrowserTest implements Runnable {
 
     @Override
     public void run() {
-        WebDriver driver = null;
+        driver = null;
 
         // Launch different browsers based on browserType
         switch (browserType.toLowerCase()) {
@@ -42,7 +45,11 @@ public class BrowserTest implements Runnable {
             driver.get("https://demo.aspnetawesome.com/");
             System.out.println(browserType + " Browser - Title: " + driver.getTitle());
             BrowserActions.takeScreenshot(driver);
-        } finally {
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        finally {
             if (driver != null) {
                 driver.quit();
             }
--- src/test/java/org/sel/com/ParallelBrowserTest.java
+++ src/test/java/org/sel/com/ParallelBrowserTest.java
@@ -9,6 +9,5 @@ public class ParallelBrowserTest {
         // Start the threads
         firefoxThread.start();
         edgeThread.start();
-        System.out.println("this is new");
     }
 }
